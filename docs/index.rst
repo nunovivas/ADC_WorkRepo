5d743307b306395829ff682058b4a37c462fa27b
Nuno Vivas <nuno.vivas@gmail.com>
2023-11-23 16:05:51 +0000

    Criada diretoria e <feita> documentação

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. Ambientes de desenvolvimento colaborativo - TP2 documentation master file, created by
+   sphinx-quickstart on Thu Nov 23 16:02:57 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Ambientes de desenvolvimento colaborativo - TP2's documentation!
+===========================================================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   clientes
+   io_terminal
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/clientes.py
+++ src/clientes.py
@@ -9,10 +9,12 @@ nome_ficheiro_lista_de_clientes = "lista_de_clientes.pk"
 # todos os outros ficheiros clientes-*.py, e inclusive estes comentários
 
 # ...
+
+#### (Esta função pergunta uma lista de clientes com os seguintes dados: Primeiro e ultimo nome, email, telemovel e nif)
 def cria_novo_cliente():
     """Pedir os dados de um novo cliente
 
-    :return: dicionario com o novo cliente, {"nome": <<nome>>, "nif": <<nif>>, ...}
+    :return: dicionario com o novo cliente, {"firstName": <<nome>>,"lastName":<<nome>>, "email",<<email>>, "mobilePhone", <<mobilePhone>>,"nif",<<nif>>, ...}
     """
     # TODO: pedir os dados do cliente e não esquecer de os devolver
     # ...
@@ -25,6 +27,7 @@ def cria_novo_cliente():
     newClient = {"firstName", primeiroNome,"lastName", ultimoNome,"email", email,"mobilePhone",telemovel,"nif",nif}
     return newClient
 
+### Imprime uma lista de cliente (que é recebida pelo tipo de dados dicionario)
 def imprime_lista_de_clientes(lista_de_clientes):
     """TODO: documentação"""
     imprime_lista(cabecalho="Lista de clientes", lista=lista_de_clientes)
