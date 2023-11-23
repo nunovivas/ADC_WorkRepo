7407041e8b59c7fc729135af981279818e91a195
Nuno Vivas <nuno.vivas@gmail.com>
2023-11-23 15:13:54 +0000

    apagados ficheiro laterais

--- clientes-imprime_lista_de_clientes.py
+++
@@ -1,8 +0,0 @@
-def imprime_lista_de_clientes(lista_de_clientes):
-    """TODO: documentação"""
-
-    #TODO: Implementar esta função
-    # ...
-    imprime_lista(cabecalho="Lista de clientes", lista=lista_de_clientes)
-
-
